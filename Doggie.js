f124a5719934808ef4a30ce6d978e4fefd8c0ed6
Rachaelj99 <rachaeljeon99@gmail.com>
2016-05-08 15:51:05 -0400

    Update Doggie.js

--- Doggie.js
+++ Doggie.js
@@ -4,9 +4,9 @@ function multiplyImage() {
   var y = '<img src="https://67.media.tumblr.com/37de4133ab335b2c3f71774da72b8b6b/tumblr_nj8f46dQJJ1rmbujlo1_400.gif">';
   var cell = "<td>" + y + "</td>";
   var row = "<tr>" + cell + "</tr>";
-  var table = "";
+  var table = x;
   for (i = 0; i<= multiply; i++){
-    table = table + x;
-    
+    table = table + row;
   }
+  table = table + "</table>";
 }
